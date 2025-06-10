83cfdb888e40270c0287024b255035486d526965
Jenny Patel <jsp5@williams.edu>
2025-06-10 14:05:11 -0400

    starting website

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// file for interactivity on website
+
+function toggleContact() {
+  const info = document.getElementById("contact-info");
+  if (info.style.display === "none") {
+    info.style.display = "block";
+  } else {
+    info.style.display = "none";
+  }
+}
 
